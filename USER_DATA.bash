e7d87472585b7c0a0089c58d11c6ba6472e20985
ChanaratThongin <chanarat.tho@dome.tu.ac.th>
2023-12-09 18:17:12 +0700

    fix: USER_DATA.bash

--- USER_DATA.bash
+++ USER_DATA.bash
@@ -1,17 +1,11 @@
 #!/bin/bash
 
 # Update the instance
-sudo yum update -y || sudo apt-get update -y
+sudo yum update -y && sudo yum upgrade -y
 
-# Install Git
-sudo yum install -y git || sudo apt-get install -y git
-
-# Install Node.js and npm for Amazon Linux 2
-sudo yum install -y nodejs npm || {
-    # For Ubuntu-based instances
-    curl -sL https://deb.nodesource.com/setup_14.x | sudo -E bash -
-    sudo apt-get install -y nodejs
-}
+# Install necessary packages
+sudo yum install -y git
+sudo yum install -y nodejs npm
 
 # Install pm2 globally using npm
 sudo npm install pm2 -g
@@ -20,19 +14,19 @@ sudo npm install pm2 -g
 cd /home/ec2-user
 
 # Clone the repository
-git clone https://github.com/ChanaratThongin/CS100-Project-G07.git
+git clone -b main https://github.com/ChanaratThongin/CS100-Project-G07.git
 
 # Navigate to the project directory and install dependencies
 cd /CS100-Project-G07/Backend
 sudo npm install
 
-Start the application using pm2
+# Start the application using pm2
 sudo pm2 start index.js
 
-Save the current pm2 processes
+# Save the current pm2 processes
 sudo pm2 save
 
-Ensure pm2 starts on boot
+# Ensure pm2 starts on boot
 sudo pm2 startup
 
 sudo yum install -y nginx
@@ -45,6 +39,8 @@ sudo cp -r /home/ec2-user/CS100-Project-G07/html /usr/share/nginx/html/
 sudo cp -r /home/ec2-user/CS100-Project-G07/css /usr/share/nginx/html/
 sudo cp -r /home/ec2-user/CS100-Project-G07/images /usr/share/nginx/html/
 sudo cp -r /home/ec2-user/CS100-Project-G07/scripts /usr/share/nginx/html/
+
+sudo nginx -s reload
 
 # Restart Nginx to reflect the changes
 sudo systemctl restart nginx
